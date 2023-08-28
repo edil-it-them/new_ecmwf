c37c6235ae330df96573c4fc0fda78e71104f689
Edil <baiyzbekovedil@gmail.com>
2023-08-29 01:01:46 +0200

    progress

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -5,10 +5,17 @@
 	import { showWaves, video, videoBorders, opacity, videoTime, recording } from '$lib/store';
 	import TimeLine from './TimeLine.svelte';
 	import { tweened } from 'svelte/motion';
+	import { useProgress } from '@threlte/extras';
 	let showCalendar = false
 	// $:{if ($videoBorders) $videoBorders.currentTime = $videoTime}
 	let twOp = tweened(1);
 	$: $opacity = $twOp;
+	const { progress } = useProgress()
+
+	const tweenedProgress = tweened($progress, {
+		duration: 800
+	})
+	$: tweenedProgress.set($progress)
 </script>
 
 <video
@@ -77,6 +84,19 @@
 		bind:value={$opacity}
 	/>
 </div>
+{#if $tweenedProgress < 1}
+	<div
+		transition:fade|local={{
+			duration: 200
+		}}
+		class="wrapper"
+	>
+		<p class="loading">Loading</p>
+		<div class="bar-wrapper">
+			<div class="bar" style="width: {$tweenedProgress * 100}%" />
+		</div>
+	</div>
+{/if}
 
 <div class="flex w-full h-full">
 	<Calendar bind:showCalendar />
@@ -88,3 +108,37 @@
 	</div>
 </div>
 <!-- Timeline -->
+
+<style>
+	.wrapper {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background-color: white;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		align-items: center;
+		justify-content: center;
+		color: black;
+	}
+
+	.loading {
+		font-size: 0.875rem;
+		line-height: 1.25rem;
+	}
+
+	.bar-wrapper {
+		width: 33.333333%;
+		height: 10px;
+		border: 1px solid black;
+		position: relative;
+	}
+
+	.bar {
+		height: 100%;
+		background-color: black;
+	}
+</style>
